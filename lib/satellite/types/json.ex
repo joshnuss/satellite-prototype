b91efd7fd5ff0d0745408892a17e0a2dc66e71ce
Josh Nussbaum <joshnuss@gmail.com>
2021-06-26 02:24:16 -0400

    Adds ecto JSON type

---
+++ lib/satellite/types/json.ex
@@ -0,0 +1,10 @@
+defmodule Satellite.Types.JSON do
+  use Ecto.Type
+
+  def type, do: :string
+
+  def cast(string) when is_binary(string), do: {:ok, string}
+  def cast(_), do: :error
+  def load(value), do: Jason.decode(value)
+  def dump(value), do: Jason.encode(value)
+end
