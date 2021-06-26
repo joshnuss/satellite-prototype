091a55aacf96b8e408b72467b1b5e2d6c7ffc8bd
Josh Nussbaum <joshnuss@gmail.com>
2021-06-26 03:28:51 -0400

    Adds Ingestion.create_log

--- lib/satellite/ingestion.ex
+++ lib/satellite/ingestion.ex
@@ -1,8 +1,13 @@
 defmodule Satellite.Ingestion do
-  alias Satellite.{ Repo, Request }
+  alias Satellite.{ Log, Repo, Request }
 
   def create_request(params) do
     Request.changeset(params)
+    |> Repo.insert()
+  end
+
+  def create_log(params) do
+    Log.changeset(params)
     |> Repo.insert()
   end
 end
--- lib/satellite/log.ex
+++ lib/satellite/log.ex
@@ -19,4 +19,11 @@ defmodule Satellite.Log do
 
     timestamps(inserted_at: :timestamp)
   end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, [:request_id, :type, :host, :app, :environment, :client, :language, :ip, :commit, :message, :data ])
+    |> validate_required([:request_id, :type, :host, :app, :environment, :client, :language, :ip, :message])
+    |> validate_inclusion(:type, ~w(info warning error debug))
+  end
 end
