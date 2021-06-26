4fa77671d136c8656d759dcb38e85186a35d927e
Josh Nussbaum <joshnuss@gmail.com>
2021-06-26 02:11:57 -0400

    Adds Log schema

---
+++ lib/satellite/Log.ex
@@ -0,0 +1,22 @@
+defmodule Satellite.Log do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key false
+  @timestamps_opts updated_at: false
+  schema "logs" do
+    field :request_id, :string
+    field :type, :string
+    field :host, :string
+    field :app, :string
+    field :environment, :string
+    field :language, :string
+    field :client, :string
+    field :ip, :string
+    field :commit, :string
+    field :message, :string
+    field :data, :string
+
+    timestamps(inserted_at: :timestamp)
+  end
+end
